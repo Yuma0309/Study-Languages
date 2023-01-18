c57d8da0e2b39616889b218c3de2b8a102e19e97
中村祐馬 <yuma.nakamura.19970309@gmail.com>
2023-01-18 11:50:57 +0900

    リスト9.16までの動作を確認

--- DbManager.php
+++ DbManager.php
@@ -5,7 +5,7 @@ function getDb() : PDO {
   $passwd = 'selfpass';
 
   $db = new PDO($dsn, $usr, $passwd);
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  // $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   // $db = new PDO($dsn, $usr, $passwd, [PDO::ATTR_PERSISTENT => true]);
   return $db;
 }
