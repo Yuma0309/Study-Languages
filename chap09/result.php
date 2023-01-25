17e8fce23e8b2d25b18747807a19485e5b1ba3ea
中村祐馬 <yuma.nakamura.19970309@gmail.com>
2023-01-26 00:01:53 +0900

    リスト9.25までの動作を確認

--- chap09/result.php
+++ chap09/result.php
@@ -22,10 +22,10 @@ try {
   $db = getDb();
   $stt = $db->prepare('SELECT * FROM book ORDER BY published DESC');
   $stt->execute();
-  while($row = $stt->fetch(PDO::FETCH_ASSOC)) {
+  // while($row = $stt->fetch(PDO::FETCH_ASSOC)) {
 
-  // $stt->setFetchMode(PDO::FETCH_ASSOC);
-  // foreach($stt as $row) { 
+  $stt->setFetchMode(PDO::FETCH_ASSOC);
+  foreach($stt as $row) { 
 ?>
   <tr>
     <td><?=e($row['isbn']) ?></td>
