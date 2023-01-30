b9da60d4409809ec9cf142922609b26ba77c9701
中村祐馬 <yuma.nakamura.19970309@gmail.com>
2023-01-30 16:26:47 +0900

    リスト9.29までの動作を確認

--- chap09/fetch_object.php
+++ chap09/fetch_object.php
@@ -4,6 +4,8 @@ require_once 'Book.php';
 
 $db = getDb();
 $stt = $db->query("SELECT * FROM book WHERE isbn='978-4-7981-4102-2'");
-if ($row = $stt->fetchObject('Book', [0.1])) {
+// if ($row = $stt->fetchObject('Book', [0.1])) {
+$stt->setFetchMode(PDO::FETCH_CLASS, 'Book', [ 0.1 ]);
+if ($row = $stt->fetch()) {
   print "{$row->title}：{$row->discount()}円";
 }
