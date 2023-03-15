c1e669828c36434770decdf183472bb0e1dce16e
中村祐馬 <yuma.nakamura.0309@gmail.com>
2023-03-16 00:11:45 +0900

    リスト10.35までの動作を確認

--- chap10/BusinessPerson.php
+++ chap10/BusinessPerson.php
@@ -1,8 +1,8 @@
 <?php
 require_once 'Person4.php';
 
-class BusinessPerson extends Person {
-// final class BusinessPerson extends Person {
+// class BusinessPerson extends Person {
+final class BusinessPerson extends Person {
   // public function work () : void{
   public final function work() : void {
     print "<p>{$this->lastName}{$this->firstName}は働いています。</p>";
