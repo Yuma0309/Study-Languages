198077d852aaef712d54cc2eb35cc0ae0d129e5f
中村祐馬 <yuma.nakamura.0309@gmail.com>
2023-04-29 08:59:44 +0900

    リスト10.79までの動作を確認

--- chap10/FriendList.php
+++ chap10/FriendList.php
@@ -12,13 +12,11 @@ class FriendList implements IteratorAggregate {
     $this->list[] = $p;
   }
 
-/*
   public function __clone() : void {
     foreach ($this->list as &$value) {
       $value = clone $value;
     }
   }
-*/
 
   public function __invoke(int $index) : mixed {
     return $this->list[$index];
