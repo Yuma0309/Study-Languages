a18538a999eac2356828386701bfc15215351e3a
中村祐馬 <yuma.nakamura.0309@gmail.com>
2023-05-30 08:15:41 +0900

    リスト11.14までの動作を確認

--- chap11/download.php
+++ chap11/download.php
@@ -3,16 +3,16 @@ $fl = $_GET['path'] ?? 'default.jpg';
 
 // if (!preg_match('/\.txt$/', $fl)) { die('不正な要求です。'); }
 
-// $flag = false;
-// $dir = new DirectoryIterator('./doc/');
-// foreach ($dir as $file) {
-//   if ($file->isFile() && $file->getFileName() === $fl) {
-//     $fl = $file->getFileName();
-//     $flag = true;
-//     break;
-//   }
-// }
-// if (!$flag) { die('不正な要求です。'); }
+$flag = false;
+$dir = new DirectoryIterator('./doc/');
+foreach ($dir as $file) {
+  if ($file->isFile() && $file->getFileName() === $fl) {
+    $fl = $file->getFileName();
+    $flag = true;
+    break;
+  }
+}
+if (!$flag) { die('不正な要求です。'); }
 
 header('Content-Type: application/octet-stream');
 header('Content-Disposition: attachment; filename = "'.$fl.'"');
